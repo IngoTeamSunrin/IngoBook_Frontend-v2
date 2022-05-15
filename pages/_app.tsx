d8b6b8f3267946fc9a57ddfc71fd47ccbd733bc1
wugusaud77 <wugusaud77@icloud.com>
2022-05-15 15:13:20 +0900

    헤더 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../stories/button.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght@300"
+            rel="stylesheet"
+          />
+          <link href="https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/static/woff2/SUIT.css" rel="stylesheet"></link>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Head>
+      </Html>
+    );
+  }
+}
